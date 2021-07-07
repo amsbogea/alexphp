7d519d0527fcc35e1e96d414f2f1b652bf2f5dd1
Alex Miranda <alex_miranda@outlook.com.br>
2021-07-07 17:19:09 -0300

    commit inical

---
+++ teste.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello Word!<br>";
+$nome = "Alex Miranda da Silva";
+$idade = 32;
+$profissao = "Técnico de informática";
+$casado = true;
+
+echo "Meu nome é $nome, tenho $idade trabalho como $profissao.<br>";
+
+echo $casado;
+
+?>
